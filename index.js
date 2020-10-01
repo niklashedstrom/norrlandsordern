28b87037fb4433ccca5bfa268d54bf3fef2ffbd4
Emil Wihlander <emil.wihlander@outlook.com>
2020-10-01 14:44:41 +0200

    Fix ports, hopefully...

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.post('/norrlands', auth.autenticated, (req, res) => {
   }
 })
 
-const server = app.listen(process.argv[2], () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   const host = server.address().address
   const port = server.address().port
   console.log("Example app listening at http://%s:%s", host, port)
