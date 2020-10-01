521ab2140c9bc132fd8cd41dbf26acb82323511d
Emil Wihlander <emil.wihlander@outlook.com>
2020-10-01 15:09:57 +0200

    Switch db file

--- knexfile.js
+++ knexfile.js
@@ -5,14 +5,14 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './db_dev.sqlite3'
+      filename: './db.sqlite3'
     },
     useNullAsDefault: true,
   },
   production: {
     client: 'sqlite3',
     connection: {
-      filename: './db_prod.sqlite3'
+      filename: './db.sqlite3'
     },
     useNullAsDefault: true,
   },
