79a7e1f0474ee34509db0c31c94de006ebe711e7
Emil Wihlander <emil.wihlander@outlook.com>
2021-03-14 22:14:15 +0100

    Restructure knex config

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,9 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
-    ssl: true,
+    connection: {
+      host: process.env.DATABASE_URL,
+      ssl: true,
+    },
   },
 };
